221476207211c8cb97967a52d75243189247d483
Hassan Askar <hassanemad15697@gmail.com>
2021-12-26 03:32:41 +0100

    making processes on login form data by using (@RequestParam)

--- src/com/spring/controller/MainController.java
+++ src/com/spring/controller/MainController.java
@@ -5,45 +5,45 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
 
 	@RequestMapping("/")
+	// http://localhost:8080/SpringMVC/
 	public String main() {
-		//http://localhost:8080/SpringMVC/
 		return "mainPage";
 	}
-	
+
 	@RequestMapping("/profile")
+	// http://localhost:8080/SpringMVC/profile
 	public String profile() {
-		//http://localhost:8080/SpringMVC/profile
 		return "myProfile";
 	}
-	
+
 	@RequestMapping("/login")
+	// http://localhost:8080/SpringMVC/login
 	public String login() {
-		//http://localhost:8080/SpringMVC/login
 		return "login";
 	}
+
 	
 	@RequestMapping("/loginProcess")
-	public String loginFormProcess(HttpServletRequest request, Model model) {
-		//http://localhost:8080/SpringMVC/loginProcess
+	// http://localhost:8080/SpringMVC/loginProcess
+	public String loginFormProcess(@RequestParam("username") String username, @RequestParam("password") String password,
+			Model model) {
 		
-		//getting parameters from login form
-		String username = request.getParameter("username");
-		String password = request.getParameter("password");
-		
-		//processing on parameters
-		username="Ciao "+username.toUpperCase();
-		password="Your Password is: "+password.toLowerCase();
-		
-		//returning processed data to new page
+		// getting parameters from login form by @RequestParam("username") String username 
+
+		// processing on parameters
+		username = "Ciao " + username.toUpperCase();
+		password = "Your Password is: " + password.toLowerCase();
+
+		// returning processed data to new page
 		model.addAttribute("processedUsername", username);
 		model.addAttribute("processedPassword", password);
-		
-		
+
 		return "myProfile";
 	}
 }
